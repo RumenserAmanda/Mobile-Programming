69b1718b28cc6a263e83238c97d45c4cb09dbc98
GilbyKoloay <lolwtf10001@gmail.com>
2022-02-14 18:27:34 +0800

    2nd commit Gilby exercise 4

--- ex4.js
+++ ex4.js
@@ -41,3 +41,54 @@
 // }
 
 // ambilDataUser3();
+
+// //4. Class
+// //a & b
+// class Orang{
+//     constructor(nama, umur){
+//         this.nama = nama;
+//         this.umur = umur;
+//     }
+//     //c
+//     bekerja(nama) {
+//         console.log(`${this.nama} sedang bekerja seperti biasa`);
+//     }
+// }
+// //d
+// let user1 = new Orang("Rumenser, Amanda", 19);
+// //e
+// console.log(user1.bekerja());
+
+
+// //5. Inheritance
+// class Orang5{//a
+//     constructor(nama, umur){
+//         this.nama = nama;
+//         this.umur = umur;
+//     }
+
+//     //b
+//     tidur(){
+//         return `${this.nama} sedang tidur`;
+//     }
+
+//     //c
+//     makan(){
+//         return `${this.nama} sedang makan`;
+//     }
+// }
+
+// class Pelajar extends Orang5 { //c
+//     constructor(nama, umur, namaSekolah) { //d
+//         super(nama, umur); //e
+//         this.namaSekolah; //f
+//         this.namaSekolah = namaSekolah;
+//     }
+
+//     belajar(){
+//         return `${this.nama} belajar di ${this.namaSekolah}`;
+//     }
+// }
+
+// const user = new Pelajar("John", 17, "Unklab");
+// console.log(user.belajar());
