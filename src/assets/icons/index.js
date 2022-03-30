c460a1e1e76c8abc9b5828bb4ae187f558af7cc4
FriendlyMaabuat <friendlymaabuat25@gmail.com>
2022-03-30 11:51:54 +0800

    Slicing Sign and Sign Up Page

---
+++ src/assets/icons/index.js
@@ -0,0 +1,4 @@
+import Logo from './logo.svg';
+import IconBack from './icon-back.svg';
+
+export {Logo, IconBack};
---
+++ src/pages/SplashScreen/index.js
@@ -0,0 +1,35 @@
+import React, {useEffect} from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import Logo from '../../assets/icons/logo.svg';
+import {SvgProps} from 'react-native-svg';
+
+
+const SplashScreen = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.replace('SignIn');
+    }, 3000);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Logo />
+      <Text style={styles.text}>Money Tracker</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#02CF8E',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 32,
+    fontFamily: 'Poppins-Regular',
+  },
+});
+
+export default SplashScreen;
